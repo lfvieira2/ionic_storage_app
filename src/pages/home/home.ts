fbc478abc89ffd505ec9859163c831a29fea8490
Luiz Filipe Vieira <luiz.filipe.vieira@hotmail.com>
2019-11-24 09:51:27 -0300

    Recuperando dados do Storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { StorageProvider } from '../../providers/storage/storage';
+import { Contact } from '../../model/contact';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  contacts: Contact[] = [];
 
   constructor(
     public navCtrl: NavController, 
     private storageProvider: StorageProvider
   ) {
-    this.storageProvider.set('contact', {name: 'Teste', phone: 41231, email: 'teste@email'});
+    this.storageProvider.getAll()
+    .then((contacts) => this.contacts = contacts)
+    .catch((err) => alert(err));
   }
 
 }
--- src/providers/storage/storage.ts
+++ src/providers/storage/storage.ts
@@ -8,7 +8,21 @@ export class StorageProvider {
   constructor(public storage: Storage) {
   }
 
-  set(key, value: Contact){
+  getAll(): Promise<Contact[]> {
+    let contacts: Contact[] = [];
+
+    return this.storage.forEach(function (contact: Contact, key: string, i: number) {
+      contacts.push(contact);
+    })
+    .then(() => Promise.resolve(contacts))
+    .catch(() => Promise.reject('Erro ao recuperar dados do storage'));
+  }
+
+  set(key, value: Contact) {
     this.storage.set(key, value);
+  }
+
+  get(key) {
+    return this.storage.get(key);
   }
 }
